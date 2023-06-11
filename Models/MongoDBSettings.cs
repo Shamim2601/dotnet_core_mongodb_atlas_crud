be716966c3f268ebdbf660388dc02b9827179c85
Shamim2601 <shamim2601@gmail.com>
2023-06-12 03:13:02 +0600

    init

---
+++ Models/MongoDBSettings.cs
@@ -0,0 +1,10 @@
+namespace firstwebapi.Models;
+
+public class MongoDBSettings {
+
+    public string ConnectionURI { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+
+}
+// }
